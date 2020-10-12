1f4ffb49b72dde348ced5d1a84a8998e7fdab1b2
Inzemam Khan <inzemamk1997@gmail.com>
2020-10-12 11:49:00 +0530

    Reformatted Code and followed Some of the good practises.

--- app.js
+++ app.js
@@ -1,69 +1,92 @@
-const express = require('express'); //Fast, unopinionated, minimalist web framework for Node.js
-const jwt = require('jsonwebtoken'); // JWT for authorization
+const express = require("express"); //Fast, unopinionated, minimalist web framework for Node.js
+const jwt = require("jsonwebtoken"); // JWT for authorization
 
 const app = express();
+const mySecretKey = "I am a secret. Don't share me at all!";
 
-app.get('/api', (req, res) => {
-    res.json({
-        message: 'Welocme to the API'
-    });
-});
-
-
-//Route that i want to protect using JWT authorization
-app.post('/api/posts', verifyToken, (req, res) => {
-    jwt.verify(req.token, 'secretkey', (err, authData) => {
-        if (err) {
-            res.sendStatus(403);
-        } else {
-            res.json({
-                message: 'Post Created ....',
-                authData
-            });
-        }
-    });
-   
-});
-
-
-//Implement jsonwebtoken.
-//Route to generate jsonwebtoken
-app.post('/api/login', (req, res) => {
-        //Mock user(In real application the request will go through a database and we will get our user back)
-        //payload 
-        const user = {
-            id: 1,
-            username: 'Inzi',
-            email: 'inzirocks@gmail.com'
-        }
-
-    jwt.sign({ user }, 'secretkey', {expiresIn : '60s'} ,(err, token) => {
-           res.json({
-               //We will get our JWT from here
-               token
-           });
-       });
-});
+app.use(express.json());
 
 //FORMAT OF TOKEN
 //Authorization : Bearer <access_token>
 //verifyToken
-function verifyToken(req,res,next) {
+function verifyToken(req, res, next) {
     //Get auth header value
-    const bearerHeader = req.headers['authorization'];
+    // 'x-auth-token' should be send by the client in the header
+    const authToken = req.headers["x-auth-token"];
     //Check if bearer is undefined
-    if (typeof bearerHeader !== 'undefined') {
-        //Split at the space
-        const bearer = bearerHeader.split(' ');
-        //Get token from array
-        const bearerToken = bearer[1];
-        //Set the token
-        req.token = bearerToken;
+    if (typeof authToken === "string") {
+        req.token = authToken;
         //Next middleware
         next();
     } else {
         //Forbidden
-        res.sendStatus(403);
+        res.status(401).send({
+            message: "Invalid token. Please login again!",
+        });
+        return;
     }
 }
-app.listen(5000, () => console.log('Server started on 5000'));
+
+app.get("/api", (req, res) => {
+    res.json({
+        message: "Welocme to the API",
+    });
+});
+
+//Route that i want to protect using JWT authorization(DATA that is private)
+app.get("/api/private", verifyToken, (req, res) => {
+    jwt.verify(req.token, mySecretKey, (err, authData) => {
+        if (err) {
+            res.status(401).send({
+                message: "Invalid token. Please login again!",
+            });
+        } else {
+            res.json({
+                message: "Private data!",
+            });
+        }
+    });
+});
+
+//Data that is public that donot need any authorization
+app.get("/api/public", (req, res) => {
+    res.json({
+        message: "Public data!",
+    });
+});
+
+//Implement jsonwebtoken.
+//Route to generate jsonwebtoken
+app.post("/api/login", (req, res) => {
+    //Mock user(In real application the request will go through a database and we will get our user back)
+    //payload
+
+    const user = req.body.userId;
+    const password = req.body.password;
+
+    if (user === "Inzi" && password === "inzi@viasat") {
+        const dataToSign = {
+            username: "Inzi",
+        };
+        //JWT generated after Login Authentication
+        //JWT token generated using userId and secret Key
+        jwt.sign(
+            { dataToSign },
+            mySecretKey,
+            { expiresIn: "240s" },
+            (err, token) => {
+                res.setHeader("x-auth-token", token);
+                res.json({
+                    message: "Successfully logged in!",
+                });
+            }
+        );
+    } else {
+        // not a valid credentials
+        res.status(400).json({
+            message: "Please check your userId and password",
+        });
+    }
+});
+
+app.listen(5000, () => console.log("Server listening on 5000"));
